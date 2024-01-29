b15e50e478189ad3e6ef6754f58d5d49ad6d4b02
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-01-29 09:19:04 -0300

    Criando Novas entidades e fazendo o segundo Migration

--- SalesWebMVC/SalesApp/Migrations/20240129121127_OtherEntities.Designer.cs
+++ SalesWebMVC/SalesApp/Migrations/20240129121127_OtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesApp.Data;
 
@@ -10,9 +11,10 @@ using SalesApp.Data;
 namespace SalesApp.Migrations
 {
     [DbContext(typeof(SalesAppContext))]
-    partial class SalesAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240129121127_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

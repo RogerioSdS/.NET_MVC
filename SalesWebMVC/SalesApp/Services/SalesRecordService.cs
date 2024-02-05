90a5c3a47b72c4419794bf04e96d63de905e5fed
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-02-05 09:09:05 -0300

    Implementando tela de buscas Agrupadas

--- SalesWebMVC/SalesApp/Services/SalesRecordService.cs
+++ SalesWebMVC/SalesApp/Services/SalesRecordService.cs
@@ -32,5 +32,33 @@ namespace SalesApp.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        //No metodo abaixo, iremos agrupar o retorno da nossa consulta, por isso estamos passando uma lista do tipo IGrouping, passando o Department e o SalesRecord como parametros
+        public async Task<List<IGrouping<Department , SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _Context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            var data = await result
+
+                .Include(x => x.Seller)
+
+                .Include(x => x.Seller.Department)
+
+                .OrderByDescending(x => x.Date)
+
+                .ToListAsync();
+
+            return data.GroupBy(x => x.Seller.Department).ToList();
+           
+        }
     }
 }
